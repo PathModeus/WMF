0db0567ea4ea06bb80dc5102a9c88696021ba5ad
Tom Bray <tom.bray@student-cs.fr>
2022-07-07 14:57:49 +0200

    add noisy function

--- src/python/filter.py
+++ src/python/filter.py
@@ -4,17 +4,21 @@ You can use weightType to modify the weight kernel of the filter.
 """
 
 import cv2 as cv
+import noise
 from matplotlib import pyplot as plt
 
 #Load the image
-img = cv.imread('img/basketball.png') 
+img = cv.imread('img/9.jpg') 
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB) #by default OpenCV uses BGR rather than RGB
 
+#Noised image
+noised = noise.noisy("s&p",img)
+
 #Filter the image
-blur = cv.ximgproc.weightedMedianFilter(img,img, 1, weightType = cv.ximgproc.WMF_EXP)
+blur = cv.ximgproc.weightedMedianFilter(noised,noised, 1, weightType = cv.ximgproc.WMF_COS)
 
 #Display the original and the filtered image
-plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.subplot(121),plt.imshow(noised),plt.title('Original')
 plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(blur),plt.title('MedianFilter')
 plt.xticks([]), plt.yticks([])
