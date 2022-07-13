89b73205d7a815279ce7f8713910b91f5bd3dafa
Tom Bray <tom.bray@student-cs.fr>
2022-07-13 14:29:12 +0200

    add a result function

--- src/python/noise.py
+++ src/python/noise.py
@@ -14,46 +14,35 @@ mode : str
 '''
 
 import numpy as np
+import numpy as np
+from skimage.util import random_noise
 
 def noisy(noise_typ,image):
 
     if noise_typ == "gauss":
-      row,col,ch= image.shape
-      mean = 0
-      var = 0.1
-      sigma = var**0.5
-      gauss = np.random.normal(mean,sigma,(row,col,ch))
-      gauss = gauss.reshape(row,col,ch)
-      noisy = image + gauss
-      return noisy
+
+      V=0.05**2 #var
+
+      noisy = random_noise(image, mode='gaussian', var=V)
 
     elif noise_typ == "s&p":
-      row,col,ch = image.shape
-      s_vs_p = 0.5
-      amount = 0.05
-      out = np.copy(image)
-      # Salt mode
-      num_salt = np.ceil(amount * image.size * s_vs_p)
-      coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
-      out[coords] = 1
+      
+      a = 0.2 #amount of noise
+      saltVsPepper = 0.5
 
-      # Pepper mode
-      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-      coords = [np.random.randint(0, i - 1, int(num_pepper))
-              for i in image.shape]
-      out[coords] = 0
-      return out
+      noisy = random_noise(image, mode='s&p', salt_vs_pepper=saltVsPepper, amount=a)
+
 
     elif noise_typ == "poisson":
-        vals = len(np.unique(image))
-        vals = 2 ** np.ceil(np.log2(vals))
-        noisy = np.random.poisson(image * vals) / float(vals)
-        return noisy
+      
+      noisy = random_noise(image, mode='poisson')
 
     elif noise_typ =="speckle":
-      row,col,ch = image.shape
-      gauss = np.random.randn(row,col,ch)
-      gauss = gauss.reshape(row,col,ch)        
-      noisy = image + image * gauss
-      return noisy
+      M = 0 #Mean
+      V = 0.05**2 #Var     
+      noisy = random_noise(image, mode='speckle', mean=M, var=V)
+    
+    noisy = (255*noisy).astype(np.uint8)
+
+    return noisy
+
--- src/python/results.py
+++ src/python/results.py
@@ -1,8 +1,7 @@
-from re import S
 import cv2 as cv
-import numpy as np
 import noise
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def similarity(img, filtered_img):
@@ -19,4 +18,19 @@ img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 noised = noise.noisy("s&p",img)
 
-similarity(img, noised)
+#Filter the image
+blur = cv.ximgproc.weightedMedianFilter(noised,noised, 1, weightType = cv.ximgproc.WMF_COS)
+
+#similarities
+noised_sim = str(round(similarity(img,noised),3))
+filtered_sim = str(round(similarity(img,blur),3))
+
+#Display the original and the filtered image
+plt.subplot(131),plt.imshow(noised),plt.title('Noised \n similarity = ' + noised_sim)
+plt.xticks([]), plt.yticks([])
+plt.subplot(132),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(blur),plt.title('Filtered \n similarity = ' + filtered_sim)
+plt.xticks([]), plt.yticks([])
+plt.show()
+
